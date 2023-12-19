0a12c4ee470c21ba095ce6853530555768183a34
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-19 18:08:15 +0100

    Added current_time() function

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use chrono::prelude::*;
 
 pub fn sleep(time: u64) {
     thread::sleep(Duration::from_secs(time));
@@ -7,4 +8,9 @@ pub fn sleep(time: u64) {
 
 pub fn ms_sleep(time: u64) {
     thread::sleep(Duration::from_millis(time));
+}
+
+fn current_time(format: String) -> String {
+    let local: DateTime<Local> = Local::now();
+    local.format(format.as_str()).to_string()
 }
