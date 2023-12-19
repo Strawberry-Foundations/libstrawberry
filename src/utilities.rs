a4e3e1414b0f62e3b02cdd7cd7ad3434d800ae04
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-19 18:12:57 +0100

    fixed private function (added pub statement)

--- src/utilities.rs
+++ src/utilities.rs
@@ -10,7 +10,7 @@ pub fn ms_sleep(time: u64) {
     thread::sleep(Duration::from_millis(time));
 }
 
-fn current_time(format: String) -> String {
+pub fn current_time(format: String) -> String {
     let local: DateTime<Local> = Local::now();
     local.format(format.as_str()).to_string()
 }
