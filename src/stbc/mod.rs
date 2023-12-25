d42800379a6e6a3316e8dbbfde59a4f67677844a
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-25 13:52:34 +0100

    Added keep_alive

--- src/stbc/mod.rs
+++ src/stbc/mod.rs
@@ -1,9 +1,10 @@
 use std::io::Write;
 use std::net::TcpStream;
+use crate::constants::KEEPALIVE_MESSAGE;
 
 pub fn keep_alive(mut stream: TcpStream, duration: u16) {
     loop {
         crate::utilities::sleep(duration as u64);
-        stream.write_all(b"[#<keepalive.event.sent>]").expect("Failed to send Keep Alive");
+        stream.write_all(KEEPALIVE_MESSAGE.as_bytes()).unwrap();
     }
 }
