6c49ad174cccaca33aafefc161f913769bb34105
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 00:50:58 +0100

    Added FeatureSet::enable + FeatureSet::disable

--- src/logging/featureset.rs
+++ src/logging/featureset.rs
@@ -2,10 +2,26 @@ pub struct FeatureSet {
     pub enable_file_handler: bool,
 }
 
+pub enum Features {
+    FileHandler,
+}
+
 impl FeatureSet {
     pub fn new() -> Self {
         Self {
             enable_file_handler: false,
+        }
+    }
+
+    pub fn enable(&mut self, feature: Features) {
+        match feature {
+            Features::FileHandler => self.enable_file_handler = true,
+        }
+    }
+
+    pub fn disable(&mut self, feature: Features) {
+        match feature {
+            Features::FileHandler => self.enable_file_handler = false,
         }
     }
 }
