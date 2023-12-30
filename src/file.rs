4a085deff33e2c14140d87f8f8fb2bac61b141a7
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 12:36:15 +0100

    Fixed Clippy warnings

--- src/file.rs
+++ src/file.rs
@@ -8,15 +8,28 @@ pub struct FileStruct {
 }
 
 impl FileHandler {
+    /// # Panics
+    ///
+    /// - Will panic if file is not readable
+    /// - Will panic if your current user does not have the
+    /// required permissions to open the file
+
+    #[must_use]
     pub fn read_file(file_path: &str) -> String {
         let mut file = File::open(file_path).expect("Error while opening file");
 
         let mut content = String::new();
         file.read_to_string(&mut content)
-            .expect("TODO: panic message");
+            .expect("Error while reading from file");
 
         content
     }
+
+    /// # Panics
+    ///
+    /// - Will panic if file is not writeable
+    /// - Will panic if your current user does not have the
+    /// required permissions to open the file
 
     pub fn write_to_file(file_path: &str, content: &str) {
         let mut file = File::create(file_path).expect("Error while creating or opening file");
@@ -27,6 +40,13 @@ impl FileHandler {
 }
 
 impl FileStruct {
+    /// # Panics
+    ///
+    /// - Will panic if file is not readable
+    /// - Will panic if your current user does not have the
+    /// required permissions to open the file
+
+    #[must_use]
     pub fn read(&self) -> String {
         let mut file: File = File::open(&self.file_path).expect("Error while opening file");
 
@@ -37,6 +57,12 @@ impl FileStruct {
 
         content
     }
+
+    /// # Panics
+    ///
+    /// - Will panic if file is not writeable
+    /// - Will panic if your current user does not have the
+    /// required permissions to open the file
 
     pub fn write(&self, content: &str) {
         let mut file = File::create(&self.file_path).expect("Error while creating or opening file");
