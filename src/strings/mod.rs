a2dc006c419c252acdf5610a7e9af41fa3bb69c3
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 12:50:19 +0100

    Fixed Clippy warnings

--- src/strings/mod.rs
+++ src/strings/mod.rs
@@ -6,11 +6,21 @@ pub struct Strings {
     pub lang_str_object: Value,
 }
 
+/// # Panics
+///
+/// - Will panic when file cannot be read as string
+
+#[must_use]
 pub fn load_language_file(path: &str) -> String {
     fs::read_to_string(path).expect("Could not read language strings")
 }
 
 impl Strings {
+    /// # Panics
+    ///
+    /// - Will panic when serde couldnt convert to yaml from str
+
+    #[must_use]
     pub fn new(language: &str, lang_strings: &str) -> Self {
         let lang_object = serde_yaml::from_str(lang_strings).unwrap();
 
@@ -20,6 +30,11 @@ impl Strings {
         }
     }
 
+    /// # Panics
+    ///
+    /// - Will panic when string cannot be loaded from language file
+
+    #[must_use]
     pub fn str(&self, string: &str) -> String {
         let string = &self.lang_str_object[&self.language][string].as_str();
         string.unwrap().to_string()
