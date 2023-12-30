6c09f6148d1a13aa91ee195692afd444672f9412
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 12:51:27 +0100

    Fixed Clippy warnings

--- src/stbc/mod.rs
+++ src/stbc/mod.rs
@@ -2,9 +2,12 @@ use std::io::Write;
 use std::net::TcpStream;
 use crate::constants::KEEPALIVE_MESSAGE;
 
+/// # Panics
+///
+/// - Will panic when stream is no longer open/wirteable
 pub fn keep_alive(mut stream: TcpStream, duration: u16) {
     loop {
-        crate::utilities::sleep(duration as u64);
+        crate::utilities::sleep(u64::from(duration));
         stream.write_all(KEEPALIVE_MESSAGE.as_bytes()).unwrap();
     }
 }
