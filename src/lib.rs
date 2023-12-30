907b968ccba00d50f86a23ff5de1af90a1eda0f9
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 12:47:12 +0100

    Added allow for clippy::should_implement_trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![warn(clippy::all, clippy::nursery, clippy::pedantic)]
-#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::module_name_repetitions, clippy::should_implement_trait)]
+#![allow(dead_code)]
 
 pub mod file;
 pub mod utilities;
