e4f438671f5ae48bd6d53c1a952adf929054e990
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 12:38:34 +0100

    Fixed Clippy warnings

--- src/utilities.rs
+++ src/utilities.rs
@@ -14,20 +14,24 @@ pub fn ms_sleep(time: u64) {
     thread::sleep(Duration::from_millis(time));
 }
 
+#[must_use]
 pub fn current_time(format: &str) -> String {
     let local: DateTime<Local> = Local::now();
     local.format(format).to_string()
 }
 
+#[must_use]
 pub fn type_of<T>(_: T) -> &'static str {
     type_name::<T>()
 }
 
+#[must_use]
 pub fn escape_ansi(string: &str) -> String {
     let ansi_escape = Regex::new(r"(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]").unwrap();
     ansi_escape.replace_all(string, "").to_string()
 }
 
+#[must_use]
 pub fn contains_whitespace(string: &str) -> bool {
     for c in string.chars() {
         if c == ' ' {
@@ -37,6 +41,7 @@ pub fn contains_whitespace(string: &str) -> bool {
     false
 }
 
+#[must_use]
 pub fn is_empty_or_whitespace(string: &str) -> bool {
-    string.chars().all(|c| c.is_whitespace())
+    string.chars().all(char::is_whitespace)
 }
