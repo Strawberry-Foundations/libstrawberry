219a6c64458e88c07dfc21c2e133befd32be038b
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 12:48:12 +0100

    Fixed Clippy warnings

--- src/fmt.rs
+++ src/fmt.rs
@@ -3,7 +3,10 @@ use serde_json::Value;
 pub struct JSON;
 
 impl JSON {
-    #![allow(dead_code)]
+    /// # Errors
+    ///
+    /// - Will return `Err` if text object cannot be converted to json
+
     pub fn from_str(string: &str) -> Result<Value, serde_json::Error> {
         serde_json::from_str(string)
     }
