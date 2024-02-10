2e3d4122ddce0df28c21753d98c0b107ddcdf8dc
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-10 16:56:49 +0000

    Added stblib-email

---
+++ src/email/server.rs
@@ -0,0 +1,29 @@
+#![allow(clippy::needless_pass_by_value)]
+
+use lettre::SmtpTransport;
+use crate::email::credentials::Credentials;
+
+pub struct Server {
+    pub address: String,
+    pub port: u16,
+    pub mailer: SmtpTransport,
+}
+
+impl Server {
+    /// # Panics
+    /// Will panic if smtp server is not reachable
+    pub fn new(address: impl ToString, port: u16, credentials: Credentials) -> Self {
+        let cred_lettre = lettre::transport::smtp::authentication::Credentials::new(credentials.email, credentials.password);
+
+        let mailer = SmtpTransport::relay(address.to_string().as_str())
+            .unwrap()
+            .credentials(cred_lettre)
+            .build();
+
+        Self {
+            address: address.to_string(),
+            port,
+            mailer
+        }
+    }
+}
