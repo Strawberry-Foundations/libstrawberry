f9f0f228f5e5bd6431df4a38325105826c889862
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-10 16:56:44 +0000

    Added stblib-email

---
+++ src/email/message.rs
@@ -0,0 +1,14 @@
+#[derive(Default)]
+pub enum ContentType {
+    #[default]
+    PLAIN,
+    HTML,
+}
+
+#[derive(Default)]
+pub enum Content {
+    #[default]
+    Default,
+    Plain(&'static str),
+    Html(&'static str)
+}
