3829a24f66af3d18b36fbc5b1c8620d96dc18e1a
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-10 15:45:54 +0000

    (currently non-working) notification library

---
+++ src/notifications/notifiers/linux.rs
@@ -0,0 +1,13 @@
+use crate::notifications::notifiers::NotifierBackend;
+
+pub struct LinuxNotifier;
+
+impl NotifierBackend for LinuxNotifier {
+    fn new() {
+
+    }
+
+    fn notification_send() {
+
+    }
+}
