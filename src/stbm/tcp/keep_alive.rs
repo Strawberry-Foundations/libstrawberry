520e8f9ef7d411dbf46a0f73cc70a8bbce096a84
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-17 12:16:49 +0100

    added stbm/tcp/keep_alive

---
+++ src/stbm/tcp/keep_alive.rs
@@ -0,0 +1,13 @@
+use std::io::Write;
+use std::net::TcpStream;
+use crate::constants::KEEPALIVE_MESSAGE;
+
+/// # Panics
+///
+/// - Will panic when stream is no longer open/writable
+pub fn keep_alive(mut stream: TcpStream, duration: u16) {
+    loop {
+        crate::utilities::sleep(u64::from(duration));
+        stream.write_all(KEEPALIVE_MESSAGE.as_bytes()).unwrap();
+    }
+}
