ed12998ef4ff2eb560b8364f77669c58149909b6
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-17 12:58:40 +0100

    clippy

---
+++ src/stbm/stbchat/packet.rs
@@ -0,0 +1,71 @@
+#![allow(clippy::future_not_send)]
+
+
+use serde::{Deserialize, Serialize};
+use crate::stbm::stbchat::object::User;
+
+/// # A packet sent from the server to the client (Server -> Client)
+/// - `SystemMessage`: A message sent from the system
+/// - `UserMessage`: A message sent from a user
+/// - `Notification`: Tells the client to show a notification
+/// - `Backend`: Sends the username to the client
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "packet_type")]
+pub enum ClientsidePacket {
+    #[serde(rename = "system_message")]
+    SystemMessage {
+        message: Message
+    },
+    #[serde(rename = "user_message")]
+    UserMessage {
+        author: User,
+        message: Message,
+    },
+    #[serde(rename = "notification_backend")]
+    Notification {
+        title: String,
+        username: String,
+        avatar_url: String,
+        content: String,
+        bell: bool,
+    },
+    #[serde(rename = "stbchat_event")]
+    Event {
+        event_type: String,
+    },
+    #[serde(rename = "stbchat_backend")]
+    Backend {
+        user_meta: UserMeta
+    }
+}
+
+/// # A packet sent from the client to the server (Client -> Server)
+/// - `Login`: A event packet for receiving the users credentials
+/// - `Message`: A message sent from client
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "packet_type")]
+pub enum ServersidePacket {
+    Login {
+        username: String,
+        password: String
+    },
+    Message {
+        message: Message
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Message {
+    pub content: String,
+}
+
+impl Message {
+    pub fn new(msg: impl ToString) -> Self {
+        Self { content: msg.to_string() }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserMeta {
+    pub username: String,
+}
