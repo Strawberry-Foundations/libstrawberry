232f0b0408e0ca93b26d546e4ea8970423140ead
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-13 23:12:01 +0200

    doc: Added more documentation

--- src/stbm/stbchat/error.rs
+++ src/stbm/stbchat/error.rs
@@ -1,5 +1,6 @@
 use thiserror::Error;
 
+/// Error in case if the package size is too large
 #[derive(Error, Debug)]
 pub enum CommunicationError {
     #[error("Packet size too large, expected <=65535, got {0}")]
