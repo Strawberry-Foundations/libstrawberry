e2389fe76f1b093c782b7292aaa6c82f8211c888
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-13 23:08:30 +0200

    doc: Added more documentation

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -8,12 +8,14 @@ use crate::logging::featureset::FeatureSet;
 use crate::utilities::current_time;
 use crate::logging::level::LogLevel;
 
+/// A simple console logger struct with custom formatting and more 
 pub struct Logger {
     pub feat_set: FeatureSet,
     pub formatting: LogFormat,
 }
 
 impl Logger {
+    /// Create a new Logger object
     #[must_use]
     pub const fn new(feature_set: FeatureSet, formatter: LogFormat) -> Self {
         Self {
@@ -22,6 +24,7 @@ impl Logger {
         }
     }
 
+    /// Will parse some logging things >.>
     fn loglevel_parser(&self, level: &LogLevel) -> String {
         let level_str = match level {
             LogLevel::DEFAULT => "DEFAULT",
@@ -39,6 +42,7 @@ impl Logger {
         }
     }
 
+    /// Will parse various placeholders that can be used by custom logging formats
     fn parse(&self, level: &LogLevel, content: impl ToString) -> String {
         match level {
             LogLevel::DEFAULT => {
@@ -85,40 +89,49 @@ impl Logger {
         }
     }
 
+    /// Default log function
     pub fn default(&self, log_message: impl Display) {
         println!("{}", self.parse(&LogLevel::DEFAULT, log_message));
     }
-
+    
+    /// Info log function
     pub fn info(&self, log_message: impl Display) {
         println!("{}", self.parse(&LogLevel::INFO, log_message));
     }
 
+    /// Error log function
     pub fn error(&self, log_message: impl Display) {
         println!("{}", self.parse(&LogLevel::ERROR, log_message));
     }
 
+    /// Warning log function
     pub fn warning(&self, log_message: impl Display) {
         println!("{}", self.parse(&LogLevel::WARNING, log_message));
     }
-
+    
+    /// Critical log function
     pub fn critical(&self, log_message: impl Display) {
         println!("{}", self.parse(&LogLevel::CRITICAL, log_message));
     }
 
+    /// Panic log function
     pub fn panic(&self, log_message: impl Display) {
         println!("{}", self.parse(&LogLevel::PANIC, log_message));
     }
 
+    /// Panic log function which will exit with exit code 1
     pub fn error_panic(&self, log_message: impl Display) -> ! {
         println!("{}", self.parse(&LogLevel::ERROR, log_message));
         std::process::exit(1);
     }
 
+    /// Critical log function which will exit with exit code 1
     pub fn critical_panic(&self, log_message: impl Display) -> ! {
         println!("{}", self.parse(&LogLevel::CRITICAL, log_message));
         std::process::exit(1);
     }
 
+    /// Panic log function which will exit with exit code 1
     pub fn panic_crash(&self, log_message: impl Display) -> ! {
         println!("{}", self.parse(&LogLevel::PANIC, log_message));
         std::process::exit(1);
