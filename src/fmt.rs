620e26e8199fe7e6bf9e89a37596091ab5c88640
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-13 22:58:29 +0200

    proj: added deprecation notice

--- src/fmt.rs
+++ src/fmt.rs
@@ -1,8 +1,8 @@
 use serde_json::Value;
 
+#[deprecated(since = "0.5.16", note = "This feature has been deprecated as it is no longer used and has no purpose.")]
 pub struct JSON;
 
-#[deprecated(since = "0.5.16", note = "This feature has been deprecated as it is no longer used and has no purpose. ")]
 impl JSON {
     /// JSON Reader to read data from a string and parse it as json data    
     /// This feature has been deprecated as it is no longer used and has no purpose.
@@ -10,7 +10,7 @@ impl JSON {
     /// # Errors
     ///
     /// - Will return `Err` if text object cannot be converted to json
-    #[deprecated(since = "0.5.16", note = "This feature has been deprecated as it is no longer used and has no purpose. ")]
+    #[deprecated(since = "0.5.16", note = "This feature has been deprecated as it is no longer used and has no purpose.")]
     pub fn from_str(string: &str) -> Result<Value, serde_json::Error> {
         serde_json::from_str(string)
     }
