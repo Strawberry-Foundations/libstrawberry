068f5265877e9efa15dd7912b0328caa459b6811
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-13 23:29:03 +0200

    impl: Allowed deprecated

--- src/fmt.rs
+++ src/fmt.rs
@@ -3,6 +3,7 @@ use serde_json::Value;
 #[deprecated(since = "0.6.0", note = "This feature has been deprecated as it is no longer used and has no purpose.")]
 pub struct JSON;
 
+#[allow(deprecated)]
 impl JSON {
     /// JSON Reader to read data from a string and parse it as json data
     /// This feature has been deprecated as it is no longer used and has no purpose.
