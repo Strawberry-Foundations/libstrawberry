70f780e370db918ab5c4a196ea4b45cf283b13c4
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-13 23:09:38 +0200

    doc: Added more documentation

--- src/logging/featureset.rs
+++ src/logging/featureset.rs
@@ -1,3 +1,4 @@
+/// Struct for creating a custom featureset for stblib::logger
 pub struct FeatureSet {
     pub enable_file_handler: bool,
 }
@@ -7,6 +8,7 @@ pub enum Features {
 }
 
 impl FeatureSet {
+    /// Create a new featureset object
     #[must_use]
     pub const fn new() -> Self {
         Self {
@@ -14,20 +16,23 @@ impl FeatureSet {
         }
     }
 
+    /// Enable a feature
     pub fn enable(&mut self, feature: &Features) {
         match feature {
             Features::FileHandler => self.enable_file_handler = true,
         }
     }
 
+    /// Disable a feature
     pub fn disable(&mut self, feature: &Features) {
         match feature {
             Features::FileHandler => self.enable_file_handler = false,
         }
     }
 
+    /// return self
     #[must_use]
-    pub const fn target_return(&self) -> &Self {
+    pub const fn unwrap(&self) -> &Self {
         self
     }
 }
