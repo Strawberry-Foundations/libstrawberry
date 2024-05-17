3c11a470d6e33b7d4d6456528398f1701f0820e3
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-17 18:31:05 +0200

    impl: Added strawberry id creds saver

--- src/id/credentials.rs
+++ src/id/credentials.rs
@@ -5,8 +5,8 @@ use crate::id::error::CredentialsError;
 
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 pub struct StrawberryIdCredentials {
-    pub username: Option<String>,
-    pub token: Option<String>,
+    pub username: String,
+    pub token: String,
 }
 
 impl StrawberryIdCredentials {
@@ -29,7 +29,36 @@ impl StrawberryIdCredentials {
         }
     }
 
-    pub fn unwrap(self) -> (String, String) {
-        (self.username.clone().unwrap(), self.token.unwrap())
+    pub fn save(username: String, token: String) -> eyre::Result<()> {
+        if let Some(home_dir) = dirs::home_dir() {
+            let config_dir = home_dir.join(".config").join("strawberry-id");
+            let credentials_path = config_dir.join("credentials.yml");
+
+            if !config_dir.exists() {
+                if let Err(err) = fs::create_dir_all(&config_dir) {
+                    return Err(CredentialsError::DirectoryCreationError(err.to_string()).into())
+                }
+            }
+
+            if !credentials_path.exists() {
+                let credentials = StrawberryIdCredentials {
+                    username,
+                    token,
+                };
+
+                match serde_yaml::to_string(&credentials) {
+                    Ok(credentials_str) => {
+                        if let Err(err) = fs::write(&credentials_path, credentials_str) {
+                            return Err(CredentialsError::WriteError(err.to_string()).into())
+                        }
+                        return Ok(())
+                    }
+                    Err(err) => return Err(CredentialsError::SerializeError(err.to_string()).into()),
+                }
+            }
+            return Err(CredentialsError::AlreadyExists.into())
+
+        }
+        return Err(CredentialsError::HomeNotFound.into())
     }
 }
