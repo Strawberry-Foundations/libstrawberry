17a5f472a6a33011456c85b94b56ca00850fc2d8
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-17 18:31:07 +0200

    impl: Added strawberry id creds saver

--- src/id/error.rs
+++ src/id/error.rs
@@ -5,5 +5,20 @@ pub enum CredentialsError {
     #[error("credentials.yml does not exist")]
     MissingCredentials,
     #[error("user home directory not found")]
-    HomeNotFound
+    HomeNotFound,
+    #[error("couldn't create directory")]
+    DirectoryCreationError(String),
+    #[error("couldn't write to file")]
+    WriteError(String),
+    #[error("couldn't serialize data")]
+    SerializeError(String),
+    #[error("couldn't serialize data")]
+    AlreadyExists,
+
+}
+
+#[derive(Error, Debug)]
+pub enum VerifierError {
+    #[error("invalid user credentials")]
+    InvalidCredentials,
 }
