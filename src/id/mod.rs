aa58272eed70522224c0131afbc834519688d958
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-17 18:08:32 +0200

    impl: Added strawberry id verifier

--- src/id/mod.rs
+++ src/id/mod.rs
@@ -1,2 +1,12 @@
-mod credentials;
-mod error;
+pub mod credentials;
+pub mod error;
+pub mod verifier;
+
+#[derive(Debug, Default, Clone)]
+pub struct StrawberryId {
+    pub email: String,
+    pub full_name: String,
+    pub profile_picture: String,
+    pub username: String,
+    pub token: String,
+}
