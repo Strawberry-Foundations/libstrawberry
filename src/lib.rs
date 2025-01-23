a306daaad37bfa34c3f4d434e674a29b5c0db0af
Juliandev02 <julian@strawberryfoundations.org>
2025-01-23 19:05:38 +0100

    refactor: reorder module declarations for improved organization

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,21 @@
 #![allow(clippy::module_name_repetitions, clippy::should_implement_trait)]
 #![allow(dead_code)]
 
-pub mod file;
-pub mod utilities;
-pub mod networking;
 pub mod colors;
-pub mod strings;
-pub mod logging;
 pub mod constants;
-pub mod stbchat;
 pub mod email;
-pub mod id;
 pub mod external;
+pub mod file;
+pub mod id;
+pub mod logging;
+pub mod networking;
+pub mod stbchat;
+pub mod strings;
+pub mod utilities;
 
-#[cfg(feature = "notifications")] pub mod notifications;
-#[cfg(feature = "stbchat-scapi")] pub mod scapi;
+#[cfg(feature = "notifications")]
+pub mod notifications;
+#[cfg(feature = "stbchat-scapi")]
+pub mod scapi;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
