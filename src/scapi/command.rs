572faea08f88c21422bc117bd98101640bc02fee
Juliandev02 <julian@strawberryfoundations.org>
2025-09-10 19:29:35 +0200

    refactor(command): remove Hash, PartialEq, and Eq from Command struct

--- src/scapi/command.rs
+++ src/scapi/command.rs
@@ -3,7 +3,7 @@ use crate::scapi::context::Context;
 pub type BoxFuture<T> = std::pin::Pin<Box<dyn std::future::Future<Output = T> + Send>>;
 pub type CommandResponse = Result<Option<String>, String>;
 
-#[derive(Hash, PartialEq, Eq, Clone)]
+#[derive(Clone)]
 pub struct Command {
     /// Name of command (execution name, e.g. test -> /test)
     pub name: String,
@@ -16,4 +16,22 @@ pub struct Command {
 
     /// Logic of command (function)
     pub handler: fn(Context) -> BoxFuture<CommandResponse>,
+}
+
+impl PartialEq for Command {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name &&
+        self.aliases == other.aliases &&
+        self.description == other.description
+    }
+}
+
+impl Eq for Command {}
+
+impl std::hash::Hash for Command {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+        self.aliases.hash(state);
+        self.description.hash(state);
+    }
 }
